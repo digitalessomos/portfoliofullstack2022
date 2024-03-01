d4c77cd995a15c36529b409fcd5e3f85c65e8f6e
digitalessomos <cristiandarioromero0087@gmail.com>
2024-03-01 18:49:02 -0300

    finishing almost

--- portfolio_romero/src/app/app-routing.module.ts
+++ portfolio_romero/src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { PortfolioComponent } from "./componentes/portfolio/portfolio.component";
-import { IniciarSesionComponent } from "./componentes/iniciar-sesion/iniciar-sesion.component";
-  
-import { AuthGuard } from './servicios/auth.guard';
 
 const routes: Routes = [
   {path:'portfolio', component:PortfolioComponent},
-  /*{path:'portfolio', component:PortfolioComponent, canActivate:[GuardGuard]},*/
-  {path:'iniciar-sesion', component:IniciarSesionComponent}, 
-  {path:'', redirectTo:'iniciar-sesion',pathMatch:'full'}
+  /* {path:'iniciar-sesion', component:IniciarSesionComponent}*/
+  {path:'portfolio', component:PortfolioComponent}, 
+  {path:'', redirectTo:'portfolio',pathMatch:'full'}
 ]; 
 
 @NgModule({
